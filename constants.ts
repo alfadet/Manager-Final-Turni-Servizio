ce2280fd8041382827b0f84fd9e3c083b7b887e6
alfadet <alfadetectives@gmail.com>
2026-02-25 15:35:54 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ import { Operator } from './types';
 // Lista inizialmente vuota, gestita localmente dal manager
 export const DEFAULT_OPERATORS: Operator[] = [];
 
-export const EMAIL_RECIPIENT = 'security@alfadectives.it';
+export const EMAIL_RECIPIENT = 'amm.alfadetectives@gmail.com';
 export const WHATSAPP_RECIPIENT = '+393481260040';
 
 // Inserisci qui l'URL della tua WebApp Google Apps Script
